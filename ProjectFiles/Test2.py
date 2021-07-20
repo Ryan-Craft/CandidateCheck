6bb574db1c6677f315134f5b0afe5768ba4b101b
RyanCraft <ryan.m.j.craft@gmail.com>
2021-07-20 12:54:47 +0800

    Small additions: Changed the plot to scatter to test ability to plot data from different arrays

--- ProjectFiles/Test2.py
+++ ProjectFiles/Test2.py
@@ -1,7 +1,11 @@
 print("Hello World")
 import numpy as np
+import matplotlib.pyplot as plot
 
-array1 = np.random.randint(0,10)
-array2 = np.random.randint(0,10)
+array1 = np.random.randint(0,10,10)
+array2 = np.random.randint(0,10,10)
 
-print(array1)
+print(array1, array2)
+
+plot.scatter(array1, array2)
+plot.show()
