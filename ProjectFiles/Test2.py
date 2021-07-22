57a8a40a469f5b24b7d91a7930136d02d10fd604
RyanCraft <ryan.m.j.craft@gmail.com>
2021-07-22 20:11:43 +0800

    Added more programs to the directory from feature lab. Updated the syntax

--- ProjectFiles/Test2.py
+++ ProjectFiles/Test2.py
@@ -1,11 +1,8 @@
-print("Hello World")
-import numpy as np
-import matplotlib.pyplot as plot
+"""
+Test2.py
 
-array1 = np.random.randint(0,10,10)
-array2 = np.random.randint(0,10,10)
+Description:
+Used for testing parts of the feature extractor code
 
-print(array1, array2)
+"""
 
-plot.scatter(array1, array2)
-plot.show()
